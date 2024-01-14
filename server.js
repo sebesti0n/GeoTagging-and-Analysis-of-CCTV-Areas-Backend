2049e809952e73b709cb1304d24ebe2302f0cad8
sebesti0n <prashankumar0710@gmail.com>
2024-01-15 00:13:11 +0530

    implement cors

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express')
 const authRoutes=require('./Routes/auth')
 const camRoutes = require('./Routes/camRouter')
 const adminRoutes = require('./Routes/admin');
+const cors = require('cors')
 const bodyParser = require('body-parser');
 const app= express();
 const knex = require('knex')(require('./Configuration/DBConfig')['development']);
 const PORT = 3008;
 app.use(bodyParser.json());
-
+app.use(cors);
 app.use(bodyParser.urlencoded({extended:true}));
 app.use('/',authRoutes);
 app.use('/camera',camRoutes);
