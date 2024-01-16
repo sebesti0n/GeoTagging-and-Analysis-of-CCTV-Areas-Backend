c33e0b071fd13c5047987ab26838aaf873fc00b1
Tejas-67 <tejasjha3210@gmail.com>
2024-01-16 19:17:42 +0530

    removed react-folder

--- geotagging-backend-prashan/uno.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig, presetUno, toEscapedSelector as e } from 'unocss'
-
-export default defineConfig({
-  content: {
-    filesystem: ['**/*.{html,js,ts,jsx,tsx}']
-  },
-  presets: [presetUno()],
-  rules: []
-})
